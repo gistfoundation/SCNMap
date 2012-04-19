9f430ff6e793d337c71f85f75b3844a2fda7e01e
ianibo <ianibbo@googlemail.com>
2012-04-19 15:50:35 +0100

    Reduce logging

--- scnmap/grails-app/controllers/scnmap/HomeController.groovy
+++ scnmap/grails-app/controllers/scnmap/HomeController.groovy
@@ -30,7 +30,7 @@ class HomeController {
 
   def poi() {
     def mdb = mongoService.getMongo().getDB('comnet')
-    log.debug("Get poi...${params}");
+    // log.debug("Get poi...${params}");
 
     def entries = null;
     if ( params.lat1 && params.lat2 && params.lng1 && params.lng2 ) {
@@ -43,23 +43,9 @@ class HomeController {
 
     def result = []
     entries.each { e ->
-      log.debug("adding ${e}");
+      // log.debug("adding ${e}");
       result.add(e);
     }
-
-    //   [
-    //     id:'p1',
-    //     name:'This is point 1',
-    //     lat:53.383,
-    //     lng:-1.466
-    //   ],
-    //   [
-    //     id:'p2',
-    //     name:'This is point 2',
-    //     lat:53.385,
-    //     lng:-1.468
-    //   ]
-    // ]
 
     if ( params.callback )
       render "${params.callback}(${result as JSON})"
