0bf3d204436fd63c1b1dea9ccd9bccd96668ad5f
ianibo <ianibbo@googlemail.com>
2012-04-19 13:30:10 +0100

    updated

--- scnmap/grails-app/controllers/scnmap/ValidationController.groovy
+++ scnmap/grails-app/controllers/scnmap/ValidationController.groovy
@@ -1,6 +1,19 @@
 package scnmap
 
+import grails.converters.*
+
 class ValidationController {
 
-    def index() { }
+  def index() { 
+    def result = [result:false];
+    if ( params.field ) {
+      switch(params.field) {
+        case 'shortcode':
+          log.debug("shortcode");
+          break;
+      }
+    }
+
+    render result as JSON;
+  }
 }
